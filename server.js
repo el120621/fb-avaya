5becb1b4d4de287bbbd852acc1a44224200d7e4a
Lorem Ipsum <30864839+pareldren@users.noreply.github.com>
2022-03-07 11:52:25 +0800

    updated codes

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT);
+// Define routers
+const facebookRouter = require('./routes/facebook') ;
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/webhook', facebookRouter);
+
+// ********* Express server **********
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Now listening on port ${port}...`));

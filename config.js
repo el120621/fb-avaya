583555e684e7a132bb85f08bd1f18fb1a0e61994
Lorem Ipsum <30864839+pareldren@users.noreply.github.com>
2022-03-12 17:29:12 +0800

    init

---
+++ config.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const config = {
+    message: {
+        tryAgent: 'Please wait while we try to connect you to an agent...',
+        noAgent: 'No agents available, please try later...',
+		connectedAgent: 'You are now connected to ',
+		chatEnded: 'Chatsession is ended by agent...',
+		welcomeChatbot: 'Welcome, how can I help you?'
+    },
+	xima: {
+		apiKey: process.env.XIMA_API_KEY,
+		serverKey: process.env.XIMA_SERVER_KEY
+	},
+	aws: {
+		accessKey: process.env.AWS_ACCESS_KEY,
+		secretKey: process.env.AWS_SECRET_KEY
+	},
+	facebook: {
+		accessToken: process.env.PAGE_ACCESS_TOKEN
+	}
+}
+
+
+module.exports = config;
